384e1d4b89b8cde67ba746fad69d32c9550aa04f
Bertrand Lanson <bertrand.lanson@protonmail.com>
2024-04-17 13:07:27 +0200

    fix: ingress rules should also default to any protocol

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "openstack_networking_secgroup_rule_v2" "ingress" {
 
   description      = try(each.value.description, false) != false ? each.value.description : "Terraform managed."
   ethertype        = try(each.value.ethertype, false) != false ? each.value.ethertype : "IPv4"
-  protocol         = try(each.value.protocol, false) != false ? each.value.protocol : "tcp"
+  protocol         = try(each.value.protocol, false) != false ? each.value.protocol : null
   port_range_min   = try(each.value.port_range_min, false) != false ? each.value.port_range_min : null
   port_range_max   = try(each.value.port_range_max, false) != false ? each.value.port_range_max : null
   remote_ip_prefix = try(each.value.remote_ip_prefix, false) != false ? each.value.remote_ip_prefix : null
